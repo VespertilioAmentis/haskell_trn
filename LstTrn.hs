c5df59ed9b152be1bf5753e2a6c31da6f555e9ad
Toniy Tsvetaev <vespertilioamentis@yandex.ru>
2017-12-10 17:57:07 +0300

    EvenOnly for endless lists

--- LstTrn.hs
+++ LstTrn.hs
@@ -1,5 +1,7 @@
 module LstTrn where
 
+import Data.List
+
 testAll :: [Bool] -> [(Int, Bool)]
 testAll = zip [1..]
 
@@ -7,10 +9,13 @@ meanList :: [Double] -> Double
 meanList x = (/ fromIntegral (length x)) $ foldr (+) 0 x
 
 evenOnly :: [a] -> [a]
-evenOnly = id
+evenOnly x = foldr appendIfEven [] (zip x [1..])
+    where
+        appendIfEven (x,n) xs | mod n 2 == 0 = (x:xs)
+                              | otherwise = xs
 
 evTest1 = evenOnly [1 .. 10] == [2,4,6,8,10]
 evTest2 = evenOnly ['a'..'z'] == "bdfhjlnprtvxz"
-evTest3 = evenOnly [2, 4, 6, 8, 10]
+evTest3 = evenOnly [2, 4, 6, 8, 10] == [4, 8]
 
-allEvenTests = testAll [evTest1, evTest2]
+allEvenTests = testAll [evTest1, evTest2, evTest3]
