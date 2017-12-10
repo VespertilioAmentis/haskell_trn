182e3d5354ff75f2d3b5c0f438f5977c03f09ea6
Toniy Tsvetaev <vespertilioamentis@yandex.ru>
2017-12-11 00:24:14 +0300

    Bin to decimal

--- DataTypesTrn.hs
+++ DataTypesTrn.hs
@@ -39,15 +39,25 @@ data Bit = Zero | One deriving (Eq, Enum)
 data Sign = Minus | Plus deriving Eq
 data Z = Z Sign [Bit]
 
+instance Enum Sign where
+    succ Minus = Plus
+    pred Plus = Minus
+    fromEnum Minus = (-1)
+    fromEnum Plus = 1
+    toEnum (-1) = Minus
+    toEnum 1 = Plus
+    toEnum _ = error "This number can't be a sign"
+
 instance Show Sign where
     show Minus = "-"
-    show Plus = "+"
+    show Plus = ""
 
 instance Show Bit where
     show Zero = "0"
     show One = "1"
 
 instance Show Z where
+    show (Z a [Zero]) = show a ++ show Zero
     show (Z a b) = show a ++ (toChars $ lstToDirectOrder b)
         where
             lstToDirectOrder :: [Bit] -> [Bit]
@@ -58,68 +68,25 @@ instance Show Z where
 instance Eq Z where
     (==) (Z a b) (Z c d) = (b == d) && (a == c)
 
---toDecimal :: [Bit] -> Int
+toDecimal :: [Bit] -> Int
+toDecimal = foldr getPow 0 . zip [0..]
+    where
+        getPow (x, y) z = z + fromEnum y * 2 ^ x
+
+binToDec :: Z -> Int
+binToDec (Z a b) = fromEnum a * toDecimal b
+
+decTests = [toDecimal [Zero] == 0,
+            toDecimal [Zero, One] == 2,
+            toDecimal [One, Zero, One] == 5,
+            toDecimal [Zero, Zero, Zero, Zero, One] == 16,
+            toDecimal [One, Zero, Zero, One, One] == 25]
+
+allDecTests = testAll decTests
+            
 
 add :: Z -> Z -> Z
 add = undefined
 
 mul :: Z -> Z -> Z
 mul = undefined
-
-{-
--- Thanks to Eugene Kalashnikov for these tests
-test001 = (add (Z Plus []) (Z Plus [])) == Z Plus []
-test002 = (add (Z Plus []) (Z Plus [One])) == Z Plus [One]
-test003 = (add (Z Plus []) (Z Minus [One])) == Z Minus [One]
-
-test011 = (add (Z Plus [Zero, One, One]) (Z Plus [One])) == Z Plus [One, One, One]
-test012 = (add (Z Plus [Zero, One, One]) (Z Plus [Zero, One])) == Z Plus [Zero, Zero, Zero, One]
-test013 = (add (Z Plus [Zero, One, One]) (Z Plus [Zero, One, One])) == Z Plus [Zero, Zero, One, One]
-
-test021 = (add (Z Minus [Zero, One, One]) (Z Minus [One])) == Z Minus [One, One, One]
-test022 = (add (Z Minus [Zero, One, One]) (Z Minus [Zero, One])) == Z Minus [Zero, Zero, Zero, One]
-test023 = (add (Z Minus [Zero, One, One]) (Z Minus [Zero, One, One])) == Z Minus [Zero, Zero, One, One]
-
-test031 = (add (Z Minus [Zero, One, One]) (Z Plus [One])) == Z Minus [One, Zero, One]
-test032 = (add (Z Minus [Zero, One, One]) (Z Plus [Zero, One])) == Z Minus [Zero, Zero, One]
-test033 = (add (Z Minus [Zero, One, One]) (Z Plus [Zero, One, One])) == Z Plus []
-
-test041 = (add (Z Plus [Zero, One, One]) (Z Minus [One])) == Z Plus [One, Zero, One]
-test042 = (add (Z Plus [Zero, One, One]) (Z Minus [Zero, One])) == Z Plus [Zero, Zero, One]
-test043 = (add (Z Plus [Zero, One, One]) (Z Minus [Zero, One, One])) == Z Plus []
-
-test051 = (add (Z Plus [One]) (Z Minus [One])) == Z Plus []
-test052 = (add (Z Plus [One]) (Z Minus [One, One])) == Z Minus [Zero, One]
-test053 = (add (Z Plus [One]) (Z Minus [Zero, One])) == Z Minus [One]
-test054 = (add (Z Plus [One]) (Z Minus [Zero, Zero, Zero, One])) == Z Minus [One, One, One]
-test055 = (add (Z Plus [One]) (Z Minus [Zero, One, Zero, One])) == Z Minus [One, Zero, Zero, One]
-test056 = (add (Z Plus [Zero, One]) (Z Minus [Zero, One, One])) == Z Minus [Zero, Zero, One]
-test057 = (add (Z Plus [Zero, One]) (Z Minus [Zero, Zero, One])) == Z Minus [Zero, One]
-test058 = (add (Z Plus [One, Zero, One]) (Z Minus [Zero, One, Zero, One])) == Z Minus [One, Zero, One]
-
-
-test101 = (mul (Z Plus []) (Z Plus [])) == emptyZ
-test102 = (mul (Z Plus []) (Z Plus [One])) == emptyZ
-test103 = (mul (Z Plus []) (Z Minus [One])) == emptyZ
-test104 = (mul (Z Plus [One]) (Z Plus [])) == emptyZ
-test105 = (mul (Z Minus [One]) (Z Plus [])) == emptyZ
-
-test111 = (mul (Z Plus [One]) (Z Plus [One])) == Z Plus [One]
-test112 = (mul (Z Minus [One]) (Z Plus [One])) == Z Minus [One]
-test113 = (mul (Z Plus [One]) (Z Minus [One])) == Z Minus [One]
-test114 = (mul (Z Minus [One]) (Z Minus [One])) == Z Plus [One]
-
-test121 = (mul (Z Plus [One]) (Z Plus [Zero, One])) == Z Plus [Zero, One]
-test122 = (mul (Z Plus [Zero, Zero, One]) (Z Plus [Zero, Zero, One])) == Z Plus [Zero, Zero, Zero, Zero, One]
-
-test131 = (mul (Z Plus [One, Zero, One, Zero, One]) (Z Plus [One, One, One])) == Z Plus [One, One, Zero, Zero, One, Zero, Zero, One]
-
-
-testAdd = [test001, test002, test003, test011, test012, test013, test021, test022, test023, test031, test032, test033, test041, test042, test043, test051, test052, test053, test054, test055, test056, test057, test058]
-testMul = [test101, test102, test103, test104, test105, test111, test112, test113, test114, test121, test122, test131]
-
-testAddMul = testAdd ++ testMul
-
-zippedTests = testAll testAddMul
--}
-
