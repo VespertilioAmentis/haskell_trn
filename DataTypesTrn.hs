b05d81baa782d667dd4060ecd824ec869634b3ea
Toniy Tsvetaev <vespertilioamentis@yandex.ru>
2017-12-17 15:02:55 +0300

    Integer coords

--- DataTypesTrn.hs
+++ DataTypesTrn.hs
@@ -108,3 +108,11 @@ add x y = decToBin $ binToDec x + binToDec y
 
 mul :: Z -> Z -> Z
 mul x y = decToBin $ binToDec x * binToDec y
+
+data Coord a = Coord a a
+
+distance :: Coord Double -> Coord Double -> Double
+distance (Coord x1 y1) (Coord x2 y2) = sqrt ( (x2 - x1) ^ 2 + (y2 - y1) ^ 2 )
+
+manhDistance :: Coord Int -> Coord Int -> Int
+manhDistance = undefined
