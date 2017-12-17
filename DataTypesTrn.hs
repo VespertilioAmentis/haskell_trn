6172b7b00ccd9575fdd41d8a63aa5f5859441e15
Toniy Tsvetaev <vespertilioamentis@yandex.ru>
2017-12-17 15:08:05 +0300

    Double and Int coords

--- DataTypesTrn.hs
+++ DataTypesTrn.hs
@@ -115,4 +115,4 @@ distance :: Coord Double -> Coord Double -> Double
 distance (Coord x1 y1) (Coord x2 y2) = sqrt ( (x2 - x1) ^ 2 + (y2 - y1) ^ 2 )
 
 manhDistance :: Coord Int -> Coord Int -> Int
-manhDistance = undefined
+manhDistance (Coord x1 y1) (Coord x2 y2) = abs (x2 - x1) + abs (y2 - y1)
