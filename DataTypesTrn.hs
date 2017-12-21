5adcdf70df8da8ea60b042bf1e86c92509342ba3
Toniy Tsvetaev <vespertilioamentis@yandex.ru>
2017-12-22 01:18:02 +0300

    Avg tree val

--- DataTypesTrn.hs
+++ DataTypesTrn.hs
@@ -330,3 +330,12 @@ height (Node a b) = 1 + max (height a) (height b)
 size :: Tree a -> Int
 size (Leaf _) = 1
 size (Node a b) = 1 + size a + size b
+
+avg :: Tree Int -> Int
+avg t =
+    let (c,s) = go t
+    in s `div` c
+  where
+    go :: Tree Int -> (Int,Int)
+    go (Leaf x) = (1, x)
+    go (Node x y) = (\z r -> (fst z + fst r, snd z + snd r)) (go x) (go y)
