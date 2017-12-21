d1d5dc57270c095e92d4821f7af5aeafbb6e146b
Toniy Tsvetaev <vespertilioamentis@yandex.ru>
2017-12-22 01:01:00 +0300

    Some tree tasks

--- DataTypesTrn.hs
+++ DataTypesTrn.hs
@@ -321,3 +321,12 @@ fac' = toNat . product . enumFromTo 1 . fromNat
 
 -----------------------------------------------------------
 
+data Tree a = Leaf a | Node (Tree a) (Tree a)
+
+height :: Tree a -> Int
+height (Leaf _) = 0
+height (Node a b) = 1 + max (height a) (height b)
+
+size :: Tree a -> Int
+size (Leaf _) = 1
+size (Node a b) = 1 + size a + size b
