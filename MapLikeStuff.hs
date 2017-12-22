07faf77f3b954b404ad51d173de13d6ef1e609d2
dah_toniy <madbat@mail.ru>
2017-12-22 19:02:16 +0300

    Expressions stuff is copied

--- MapLikeStuff.hs
+++ MapLikeStuff.hs
@@ -39,5 +39,19 @@ instance Monoid a => Monoid (Maybe' a) where
     mappend x (Maybe' Nothing) = x
     mappend (Maybe' (Just x)) (Maybe' (Just y)) = Maybe' $ Just $ x `mappend` y
 
+-----------------------------------------------
+
+infixl 6 :+:
+infixl 7 :*:
+data Expr = Val Int | Expr :+: Expr | Expr :*: Expr
+    deriving (Show, Eq)
+
+expand :: Expr -> Expr
+expand ((e1 :+: e2) :*: e) = expand e1 :*: expand e :+: expand e2 :*: expand e
+expand (e :*: (e1 :+: e2)) = expand e :*: expand e1 :+: expand e :*: expand e2
+expand (e1 :+: e2) = expand e1 :+: expand e2
+expand (e1 :*: e2) = expand e1 :*: expand e2
+expand e = e
+
     
 
