4cbae6c73b5141a23d09dffea5e24bfc4aa80db6
dah_toniy <madbat@mail.ru>
2017-12-22 18:21:18 +0300

    Monoid stuff is compiled

--- MapLikeStuff.hs
+++ MapLikeStuff.hs
@@ -30,6 +30,14 @@ instance MapLike ListMap where
 
 newtype ArrowMap k v = ArrowMap { getArrowMap :: k -> Maybe v }
 
---instance MapLike ArrowMap where
+newtype Maybe' a = Maybe' { getMaybe :: Maybe a }
+    deriving (Eq,Show)
+
+instance Monoid a => Monoid (Maybe' a) where
+    mempty = Maybe' Nothing
+    mappend (Maybe' Nothing) x = x
+    mappend x (Maybe' Nothing) = x
+    mappend (Maybe' (Just x)) (Maybe' (Just y)) = Maybe' $ Just $ x `mappend` y
+
     
 
