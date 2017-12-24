657ebf078e03a2151c0fa1292fd8157e4196800a
Toniy Tsvetaev <vespertilioamentis@yandex.ru>
2017-12-24 15:50:35 +0300

    No duplicated monoid task

--- MapLikeStuff.hs
+++ MapLikeStuff.hs
@@ -30,15 +30,6 @@ instance MapLike ListMap where
 
 newtype ArrowMap k v = ArrowMap { getArrowMap :: k -> Maybe v }
 
-newtype Maybe' a = Maybe' { getMaybe :: Maybe a }
-    deriving (Eq,Show)
-
-instance Monoid a => Monoid (Maybe' a) where
-    mempty = Maybe' Nothing
-    mappend (Maybe' Nothing) x = x
-    mappend x (Maybe' Nothing) = x
-    mappend (Maybe' (Just x)) (Maybe' (Just y)) = Maybe' $ Just $ x `mappend` y
-
 -----------------------------------------------
 
 infixl 6 :+:
