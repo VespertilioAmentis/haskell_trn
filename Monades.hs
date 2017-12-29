daa904f3c8e961d87a6483f8875b4e0298a001c2
dah_toniy <madbat@mail.ru>
2017-12-29 11:19:15 +0300

    Monading stuff

---
+++ Monades.hs
@@ -0,0 +1,25 @@
+module Monades where
+
+data Log a = Log [String] a
+    deriving Show
+
+decompose (Log x y) = (x,y)
+
+toLogger :: (a -> b) -> String -> (a -> Log b)
+toLogger fn msg = Log ([msg]) . fn
+
+execLoggers :: a -> (a -> Log b) -> (b -> Log c) -> Log c
+execLoggers parm fn1 fn2 = undefined
+
+data SomeType a = SomeType a
+
+instance Applicative SomeType where
+    pure = SomeType
+    (<*>) = undefined
+
+instance Monad SomeType where
+    (>>=) = undefined
+
+
+instance Functor SomeType where
+    fmap f x = undefined
