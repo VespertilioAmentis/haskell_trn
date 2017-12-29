3b960306f5b7b446fa98557ada9da6872c7450b0
dah_toniy <madbat@mail.ru>
2017-12-29 12:07:04 +0300

    Loggers are execed, but msgs aren't concated

--- Monades.hs
+++ Monades.hs
@@ -3,13 +3,18 @@ module Monades where
 data Log a = Log [String] a
     deriving Show
 
+returnLog :: a -> Log a
+returnLog = Log []
+
 decompose (Log x y) = (x,y)
 
 toLogger :: (a -> b) -> String -> (a -> Log b)
 toLogger fn msg = Log ([msg]) . fn
 
 execLoggers :: a -> (a -> Log b) -> (b -> Log c) -> Log c
-execLoggers parm fn1 fn2 = undefined
+execLoggers parm fn1 fn2 = fn2 $ extractVal $ fn1 parm
+    where
+        extractVal (Log _ x) = x
 
 data SomeType a = SomeType a
     deriving Show
